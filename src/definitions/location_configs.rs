bc000e3dd7af96f40f1ad6b829e0f0685c8d6af4
AfrO-hash <rnoldmaturin@gmail.com>
2025-04-01 15:39:40 -0400

    Update location_configs.rs

--- src/definitions/location_configs.rs
+++ src/definitions/location_configs.rs
@@ -6,6 +6,7 @@ use std::{
 };
 use ::error::Context;
 use bytes::{Bytes, BytesMut, Buf, BufMut};
+use crate::definitions::location_configs::Buf;
 //use rs3cache_backend::buf::TryReadExt;
 use path_macro::path;
 #[cfg(feature = "pyo3")]
